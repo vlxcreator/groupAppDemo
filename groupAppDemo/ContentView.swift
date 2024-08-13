41827b5f1aa1ce0623520fd5988d1acaede58358
Fiona <fapaw@outlook.com>
2024-08-13 09:42:34 +0100

    Initial Commit

---
+++ groupAppDemo/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  groupAppDemo
+//
+//  Created by Scholar on 07/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            //Color(.systemBlue)
+            Image("sunset")
+                .resizable(resizingMode: .stretch)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("rolemodel")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .cornerRadius(32)
+                HStack {
+                    Text("Tarika Barrett")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                    Spacer()
+                    Text("GIRLS WHO CODE")
+                        .foregroundColor(Color.blue)
+                    
+                }
+                
+                Text("CEO of Girls Who Code,an international non-profit organization working to close the gender gap in technology that has served over 450,000 students to date.")
+                    .foregroundColor(Color.blue)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(15.0)
+            .shadow(radius: 40)
+            .padding()
+        
+        }
+        
+          
+        }
+    }
+
+#Preview {
+    ContentView()
+}
---
+++ groupAppDemo/groupAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupAppDemoApp.swift
+//  groupAppDemo
+//
+//  Created by Scholar on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct groupAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
